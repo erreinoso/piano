2bcdf2d4ce1960177b015633ee52c09b55312dc2
erreinoso <eramirezreinoso@gmail.com>
2020-09-17 17:12:16 +0200

    sound by mouseover and by click

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,36 @@
 'use strict';
 
+//function that add animation and sound
 function playNote(ev) {
-  const key = document.querySelector(`.piano-tone[data-key="${ev.keyCode}"]`);
-  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+  let code = '';
+  if (ev.keyCode) {
+    code = ev.keyCode;
+  } else {
+    code = ev.toElement.dataset.key;
+  }
 
+  const key = document.querySelector(`.piano-tone[data-key="${code}"]`);
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+
+  //play sound
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
 
+  //start animation
   key.classList.add('piano-tone-down');
-  console.log('playnote key', key);
-  // const keyAfter = document.querySelector(
-  //   `.piano-tone[data-key="${ev.keyCode}"]`
-  // );
-  // console.log('keyAfter', keyAfter);
+
+  // finish animation
   setTimeout(() => {
     key.classList.remove('piano-tone-down');
   }, 500);
 }
 
+//add event listerners, for mouseover,click, and keydown
 window.addEventListener('keydown', playNote);
+window.addEventListener('mouseover', playNote);
+
+const scale = document.querySelectorAll('div[data-key]');
+for (let i = 0, note; (note = scale[i]); i++) {
+  note.addEventListener('click', playNote);
+}
